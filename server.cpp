7d1871f60f31431cdfc6295ff6dea7fe858106c6
ankithshetty397 <ankithpshetty@gmail.com>
2024-09-20 17:51:43 +0530

    Implement sliding window rate limiting with IP tracking

--- server.cpp
+++ server.cpp
@@ -4,13 +4,20 @@
 #include <unistd.h>
 #include <cstring>
 #include <arpa/inet.h>
+
 #include <thread>
 #include <queue>
 #include <mutex>
 #include <condition_variable>
 #include <vector>
 
+#include <map>
+#include <chrono>
+#include <deque>
+
 #define PORT 8080
+#define REQUEST_LIMIT 5
+#define WINDOW_SIZE 10
 
 using namespace std;
 
@@ -18,21 +25,52 @@ queue<int> task_queue;
 mutex queue_mutex;
 condition_variable condition;
 
+map<string,deque<chrono::steady_clock::time_point>> request_map;
+mutex rate_limit_mutex;
+
 void handle_client(int socket_fd){
     char buffer[1024] = {0};
     const char *msg = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-length: 19\n\nHello from server";
+    const char *rate_limit_msg = "HTTP/1.1 429 Too Many Requests\nContent-Type: text/plain\nContent-length: 30\n\nRate limit exceeded";
     
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
 
     if(getpeername(socket_fd,(struct sockaddr*)&client_addr, &client_addr_len)==0){
-        char *client_ip = inet_ntoa(client_addr.sin_addr);
+        string client_ip = inet_ntoa(client_addr.sin_addr);
         cout<<"Client ip address: "<<client_ip<<endl;
+
+        {
+            lock_guard<mutex> lock(rate_limit_mutex);
+            auto now = chrono::steady_clock::now();
+
+            if(request_map.find(client_ip)!=request_map.end()){
+                auto &timestamps = request_map[client_ip];
+                while(!timestamps.empty() && chrono::duration_cast<chrono::seconds>(now-timestamps.front()).count()> WINDOW_SIZE){
+                    timestamps.pop_front();
+                }
+
+                if(timestamps.size()>=REQUEST_LIMIT){
+                    send(socket_fd, rate_limit_msg,strlen(rate_limit_msg),0);
+                    cout<<"Blocked : "<<client_ip<<endl;
+                    close(socket_fd);
+                    return;
+                }
+                timestamps.push_back(now);
+            }else{
+                request_map[client_ip] = deque<chrono::steady_clock::time_point>{now};
+            }
+        }
     }else{
         cerr<<"Failed to fetch the IP address"<<endl;
     }
 
-    read(socket_fd,buffer,1024);
+    ssize_t bytes_read = read(socket_fd,buffer,1024);
+    if(bytes_read<0){
+        cerr<<"Error reading from socket"<<endl;
+        close(socket_fd);
+        return;
+    }
     cout<<"Message received: "<<buffer<<endl;
 
     send(socket_fd,msg,strlen(msg),0);
